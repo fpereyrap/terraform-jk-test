0545024dce1775f47c90522c2ad7c016b7bb6194
fpereyrap <f.pereyrap@gmail.com>
2022-06-06 16:07:42 -0300

    test

--- test.groovy
+++ test.groovy
@@ -1,59 +1,26 @@
-// import common.Constants
-// import common.Helpers
-// def validProducts = ['p76']
+pipeline {
+  agent any
+  tools {
+      terraform "Terraform1.0.0"
+  }
 
-job("main/test") {
-    description('Runs terraform code aiming to Azure accounts')
-    wrappers {
-        maskPasswords()
-        timestamps()
-    }
-    logRotator {
-        numToKeep(500)
-    }
-    parameters {
-        textParam('TEST_PARAMETER',
-        '# sample line 1 \n\
-        # sample line 2',
-        'Jenkins testing parameters to build with parameter')
-    }
-    concurrentBuild()
-    properties {
-        throttleJobProperty {
-            throttleEnabled(true)
-            maxConcurrentPerNode(0)
-            maxConcurrentTotal(3)
-            throttleOption('project')
-            limitOneJobWithMatchingParams(true)
-            paramsToUseForLimit('')
-            matrixOptions{
-                throttleMatrixBuilds(false)
-                throttleMatrixConfigurations(false)
-            }
-        }
-    }
-    environmentVariables {
-        groovy(BUILD_NAME_SCRIPT="test.groovy")
-    }
-    scm {
-        git {
-            branch("*/${realBranchName}")
-            remote {
-                name(REPO_NAME='terraform-jk-test')
-                url('$GIT_URL')
-                // credentials(Constants.REPO_CREDS)
-            }
-            extensions {
-                cleanAfterCheckout()
-            }
-        }
+  stages {
+    stage('Git Checkout') {
+      steps {
+        url: 'https://github.com/fpereyrap/terraform-jk-test'
+      }
     }
 
-    steps {
-        shell('''ls -la
-ls -la
-terraform init
-''')
+    stage('Terraform Init') {
+      steps {
+        sh label: '', script: 'terraform init'
+      }
     }
-
+    
+    stage('Terraform apply') {
+      steps {
+        sh label: '', script: 'terraform apply --auto-approve'
+      }
+    }
+  }
 }
